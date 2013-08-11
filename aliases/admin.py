424d7d820039cbddfc3c73409ee915c8628943b7
Romain THERRAT <romain42@gmail.com>
2013-08-11 21:20:27 +0200

    Add: When create alias in admin, set owner to current user.

--- aliases/admin.py
+++ aliases/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from aliases.models import Alias
 
 class AliasAdmin(admin.ModelAdmin):
-  list_display = ('content', 'active', 'created_at')
+  list_display = ('content', 'active', 'created_at', 'created_by')
   search_fields = ['content']
-  list_filter = ('active','created_at')
+  list_filter = ('active','created_at', 'created_by')
+
+  def save_model(self, request, obj, form, change):
+    if obj.created_by_id == None:
+      obj.created_by = request.user
+    obj.save()
 
 admin.site.register(Alias, AliasAdmin)
