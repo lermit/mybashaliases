0cbb268d3893de65a0ae0e4a49fb10f9b9cc2bf1
Romain THERRAT <romain42@gmail.com>
2013-08-11 22:15:04 +0200

    Add admin action for active/unactive items

--- aliases/admin.py
+++ aliases/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from aliases.models import Alias
 
+def make_active(modeladmin, request, queryset):
+  queryset.update(active=True)
+make_active.short_description = "Mark active selected item"
+
+def make_unactive(modeladmin, request, queryset):
+  queryset.update(active=False)
+make_unactive.short_description = "Mark unactive selected item"
+
 class AliasAdmin(admin.ModelAdmin):
   list_display = ('content', 'active', 'created_at', 'created_by')
   search_fields = ['content']
   list_filter = ('active','created_at', 'created_by')
+  actions = [make_active, make_unactive]
 
   def save_model(self, request, obj, form, change):
     if obj.created_by_id == None:
